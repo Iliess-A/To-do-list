0612ae2b7bf1c570673fe5b5f60aa33a731eeb1a
iliess <iliessabdelmadjid@gmail.com>
2024-04-30 16:37:28 +0200

    css fin

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,19 @@ import { refresh } from "./refresh.js";
 const toDo_list = JSON.parse(localStorage.getItem('toDo_list')) || [];
 const add_toDo = document.getElementById('submit_toDo');
 const text_toDo =document.getElementById('add_ToDo');
-const ul_list = document.getElementById('liste');
+const ul_list = document.getElementById('list');
 
 //call the fuction to print the list from the local storage if there is something in the list
 refresh(toDo_list,ul_list);
 add_toDo.addEventListener('click',(event) =>{
 
-    let text=text_toDo.value;
-    toDo_list.push(text)
-    text_toDo.value ='';
-    console.log(toDo_list);
-    refresh(toDo_list,ul_list);
-    //apres chaque ajout il refresh le tableau en local storage
+    let text=text_toDo.value;// prend la valeur dans l'input.
+    toDo_list.push(text)//ajoute le text dans le tableau.
+    text_toDo.value ='';//retire le text de l'input.
+    // console.log(toDo_list); //debug
+    refresh(toDo_list,ul_list);// appelle la fction refresh qui vas afficher le texte dans la liste.
+    
+    //after every add/submit update the table.
     localStorage.setItem('toDo_list', JSON.stringify(toDo_list));
 });
 
